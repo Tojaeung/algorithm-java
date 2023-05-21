42fdd7f0ef28852e6484e79ac46edffc45bccff9
Tojaeung <kimwoon11@naver.com>
2023-05-21 23:58:10 +0900

    Level2. 전화번호목록

---
+++ 고득점kit/해쉬/전화번호목록/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java
+// Level 2
+class Solution {
+	public boolean solution(String[] phone_book) {
+		// 해쉬맵 만든다.
+		HashMap<String, Integer> hm = new HashMap<>();
+		for (int i = 0; i < phone_book.length; i++)
+			hm.put(phone_book[i], 1);
+
+		// 모든 전화번호의 접두어가 해쉬맵에 존재하는지 확인
+		for (String phoneNumber : phone_book) {
+			for (int i = 1; i < phoneNumber.length(); i++) {
+				if (hm.containsKey(phoneNumber.substring(0, i)))
+					return false;
+			}
+		}
+
+		// 접두어가 없으면 true 리턴
+		return true;
+	}
+}
